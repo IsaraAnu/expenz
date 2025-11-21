2f0dfdb3b55d1b73e3f6e30cd1914957c7c9be8d
IsaraAnu <isaraanuth@gmail.com>
2025-11-21 14:21:50 +0530

    JSON and DART conversion

--- lib/models/icnome_model.dart
+++ lib/models/icnome_model.dart
@@ -37,4 +37,30 @@ class Income {
     required this.time,
     required this.description,
   });
+
+  // convert income object to json object
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'amount': amount,
+      'category': category.index,
+      'date': date.toIso8601String(),
+      'time': time.toIso8601String(),
+      'description': description,
+    };
+  }
+
+  // convert json object to income object
+  factory Income.fromJson(Map<String, dynamic> json) {
+    return Income(
+      id: json['id'],
+      title: json['title'],
+      amount: json['amount'],
+      category: IncomeCategory.values[json['category']],
+      date: DateTime.parse(json['date']),
+      time: DateTime.parse(json['time']),
+      description: json['description'],
+    );
+  }
 }
